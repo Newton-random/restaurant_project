95ab16227b503c60477129b4b2256f3d6afe1172
Newton-random <kariminewton56@gmail.com>
2025-02-19 12:15:59 +0300

    Creating a restaurant website

---
+++ src/components/Feedback.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Feeedback</h1>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home_page from '@/components/Home_page.vue'
+import About from '@/components/About.vue'
+import Bookings from '@/components/Bookings.vue'
+import Contact from '@/components/Contact.vue'
+import Feedback from '@/components/Feedback.vue'
+import Menu from '@/components/Menu.vue'
+import Order from '@/components/Order.vue'
+import Restaurant_list from '@/components/Restaurant_list.vue'
+
+
+const routes = [
+    {
+    path:'/',
+    component: Home_page
+    },
+    {
+    path:'/About',
+    component: About
+    },
+    {
+    path:'/Bookings',
+    component: Bookings
+    },
+    {
+    path:'/Contact',
+    component: Contact
+    },
+    {
+    path:'/Feedback',
+    component: Feedback
+    },
+    {
+    path:'/Menu',
+    component: Menu
+    },
+    {
+    path:'/Order',
+    component: Order
+    },
+    {
+    path:'/Restaurant_list',
+    component: Restaurant_list
+    },
+]
+const router = createRouter(
+    {
+    history: createWebHistory(),
+    routes: routes,
+})
+
+export default router
